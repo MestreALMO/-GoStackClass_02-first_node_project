0bc2794a9febbded5c894c40e11bff3bf177bb86
MestreALMO <lusegardis@gmail.com>
2020-11-22 10:22:14 -0300

    done

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,24 +1,32 @@
-import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { response, Router } from 'express';
+import { parseISO } from 'date-fns';
+
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRouter.get('/', (request,response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
 appointmentsRouter.post('/', (request,response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date));
+    const parsedDate = parseISO(date);
 
-  const findAppointmentInSampleDate = appointmentsRepository.findByDate(parsedDate);
+    const createAppointment = new CreateAppointmentService(appointmentsRepository);
 
-  if (findAppointmentInSampleDate) {
-    return response.status(400).json({ message: 'This appointment is already booked' });
+    const appointment = createAppointment.execute({ provider, date: parsedDate });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
   }
-
-  const appointment = appointmentsRepository.create(provider,parsedDate);
-
-  return response.json(appointment);
-})
+});
 
 export default appointmentsRouter;
